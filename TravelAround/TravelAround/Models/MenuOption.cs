d29615d95ad6a7fa3a9210ef6cbcb73fb0cbf49e
cesarlopezt <cesarlopez2808@gmail.com>
2021-08-27 21:42:36 -0400

    HomePage

---
+++ TravelAround/TravelAround/Models/MenuOption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelAround.Models
+{
+    class MenuOption
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
--- TravelAround/TravelAround/ViewModels/HomeViewModel.cs
+++ TravelAround/TravelAround/ViewModels/HomeViewModel.cs
@@ -1,10 +1,29 @@
 ﻿ using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+using TravelAround.Models;
 
 namespace TravelAround.ViewModels
 {
     class HomeViewModel
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MenuOption BuyTicketsOption { get; set; }
+        public MenuOption CheckTrendsOption { get; set; } = new MenuOption();
+        public MenuOption FastFoodOption { get; set; } = new MenuOption();
+        public MenuOption TipsOption { get; set; } = new MenuOption();
+        public MenuOption JournalOption { get; set; } = new MenuOption();
+
+        public HomeViewModel() 
+        {
+            BuyTicketsOption = new MenuOption() { Title = "Buy a Ticket", Image = "airplane" };
+            CheckTrendsOption = new MenuOption() { Title = "Check trends", Image = "chart" };
+            FastFoodOption = new MenuOption() { Title = "Fast Food", Image = "food" };
+            TipsOption = new MenuOption() { Title = "Tips on Trips", Image = "information" };
+            JournalOption = new MenuOption() { Title = "Journal", Image = "book" };
+        }
     }
 }
